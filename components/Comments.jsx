752d3c8e3880d289e96917aa8b85d6cbaa7cc15c
kyle <kylelynch104@gmail.com>
2022-03-15 23:22:22 -0500

    Comments component

--- components/Comments.jsx
+++ components/Comments.jsx
@@ -1,6 +1,28 @@
-const Comments = () => {
+import { useState, useEffect, useRef } from 'react'
+
+const Comments = ({ slug }) => {
+  const [error, setError] = useState(false)
+  const [localStorage, setLocalStorage] = useState(null)
+  const [showSuccessMsg, setShowSuccessMsg] = useState(false)
+  const commentRef = useRef()
+  const nameRef = useRef()
+  const emailRef = useRef()
+  const storeDataRef = useRef()
+
   return (
-    <article>Comments</article>
+    <article className='px-10 py-6 rounded'>
+      <h2 className='text-xl font-semibold mb-8'>Leave a Comment</h2>
+        <form>
+          <div>
+            <textarea name="" id="" className='w-full p-4 outline-none '></textarea>
+          </div>
+        </form>
+      <style jsx>{`
+        article {
+          background-color: var(--clr-gray-100);
+        }
+      `}</style>
+    </article>
   )
 }
 export default Comments
